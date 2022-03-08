090c7c67a1ed150950c4bf4e78f000f8e679614c
Robério Almeida <dev.roberio@gmail.com>
2022-03-08 20:53:01 -0300

    feat: cria pasta e componentes necessários para configuração das rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Login, Search, Album, Favorites, Profile, ProfileEdit, NotFound } from './comp';
 
-class App extends React.Component {
+class App extends Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/comp.js
@@ -0,0 +1,7 @@
+export { default as Login } from './pages/Login';
+export { default as Search } from './pages/Search';
+export { default as Album } from './pages/Album';
+export { default as Favorites } from './pages/Favorites';
+export { default as Profile } from './pages/Profile';
+export { default as ProfileEdit } from './pages/ProfileEdit';
+export { default as NotFound } from './pages/NotFound';
