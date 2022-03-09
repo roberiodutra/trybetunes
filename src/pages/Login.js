d5b557ccdb91e9ed8f40236c426540c991d61b8d
Robério Almeida <dev.roberio@gmail.com>
2022-03-08 21:53:23 -0300

    feat: cria o formulário padrão no componente Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,27 @@ import React, { Component } from 'react';
 class Login extends Component {
   render() {
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+      <form>
+        <label htmlFor="login-name">
+          <input
+            type="text"
+            name="login-name"
+            value={ name }
+            data-testid="login-name-input"
+            onChange={ this.inputChange }
+            placeholder="What is your name?"
+          />
+        </label>
+        <input
+          type="button"
+          value="Enviar"
+          disabled={ onButton }
+          data-testid="login-submit-button"
+          onClick={ this.handleClick }
+        />
+      </form>
+    </div>
     );
   }
 }
