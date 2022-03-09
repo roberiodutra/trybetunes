1f5852ea4c3533f08bd2f86fecd2c0851297afcd
Robério Almeida <dev.roberio@gmail.com>
2022-03-09 04:21:10 -0300

    feat: cria funções para capturar historico da rota redirecionar e criar usuario na pagina de login

--- src/App.js
+++ src/App.js
@@ -3,19 +3,50 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Login, Search, Album, Favorites, Profile, ProfileEdit, NotFound } from './comp';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputName: '',
+      isButton: true,
+    };
+  }
+
+  onInputChange = ({ target: { value } }) => {
+    this.setState({ inputName: value }, this.validate);
+  }
+
+  validate = () => {
+    const { inputName } = this.state;
+    const MIN = 3;
+    return (inputName.length >= MIN)
+      ? this.setState({ isButton: false })
+      : this.setState({ isButton: true });
+  }
+
+  login = () => {
+    const { inputName, isButton } = this.state;
+    return (<Login
+      onInputChange={ this.onInputChange }
+      inputName={ inputName }
+      isButton={ isButton }
+    />);
+  }
+
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route exact path="/search" component={ Search } />
-          <Route exact path="/album/:id" component={ Album } />
-          <Route exact path="/favorites" component={ Favorites } />
-          <Route exact path="/profile" component={ Profile } />
-          <Route exact path="/profile/edit" component={ ProfileEdit } />
-          <Route path="*" component={ NotFound } />
-        </Switch>
-      </BrowserRouter>
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ this.login } />
+            <Route exact path="/search" component={ Search } />
+            <Route exact path="/album/:id" component={ Album } />
+            <Route exact path="/favorites" component={ Favorites } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route exact path="/profile/edit" component={ ProfileEdit } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </div>
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,31 +1,60 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import { Loading } from '../comp';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+    };
+  }
+
+  onButtonClick = () => {
+    const { inputName, history } = this.props;
+    this.setState({ isLoading: true },
+      async () => {
+        const create = await createUser({ name: inputName });
+        return create === 'OK' && history.push('/search');
+      });
+  }
+
   render() {
-    return (
-      <div data-testid="page-login">
-      <form>
-        <label htmlFor="login-name">
-          <input
-            type="text"
-            name="login-name"
-            value={ name }
-            data-testid="login-name-input"
-            onChange={ this.inputChange }
-            placeholder="What is your name?"
-          />
-        </label>
-        <input
-          type="button"
-          value="Enviar"
-          disabled={ onButton }
-          data-testid="login-submit-button"
-          onClick={ this.handleClick }
-        />
-      </form>
-    </div>
-    );
+    const { inputName, onInputChange, isButton } = this.props;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading />
+      : (
+        <div data-testid="page-login">
+          <form>
+            <label htmlFor="login-name">
+              <input
+                type="text"
+                name="login-name"
+                value={ inputName }
+                data-testid="login-name-input"
+                onChange={ onInputChange }
+                placeholder="What is your name?"
+              />
+            </label>
+            <input
+              type="button"
+              value="Enviar"
+              disabled={ isButton }
+              data-testid="login-submit-button"
+              onClick={ this.onButtonClick }
+            />
+          </form>
+        </div>
+      );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  inputName: PropTypes.string,
+  onInputChange: PropTypes.func,
+  isButton: PropTypes.bool,
+}.isRequired;
+
+export default withRouter(Login);
