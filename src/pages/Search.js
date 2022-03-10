c4133491369355f7e5a3f829a381c51ff39e6846
Robério Almeida <dev.roberio@gmail.com>
2022-03-10 00:19:01 -0300

    feat: cria o formulário para pesquisar artistas

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,48 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchArtist: '',
+      isButton: true,
+    };
+  }
+
+  onInputChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value }, this.validate);
+  };
+
+  validate = () => {
+    const { searchArtist } = this.state;
+    const MIN = 2;
+    return (searchArtist.length >= MIN)
+      ? this.setState({ isButton: false })
+      : this.setState({ isButton: true });
+  }
+
   render() {
+    const { isButton, searchArtist } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <div>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            value={ searchArtist }
+            name="searchArtist"
+            placeholder="Artist Name"
+            onChange={ this.onInputChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isButton }
+          >
+            Search
+          </button>
+        </div>
       </div>
     );
   }
